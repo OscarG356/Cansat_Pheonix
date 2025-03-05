162e45f4486738d1105b7cde508c6ae051f57ea2
Oscar Gutierrez <oscar.gutierrezr@udea.edu.co>
2025-03-05 17:30:43 -0500

    Se añaden las imagenes y reloj funional, falta programar el selector de puerto y baudrate

--- Cansat_Pheonix/Form1.Designer.cs
+++ Cansat_Pheonix/Form1.Designer.cs
@@ -28,20 +28,219 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.Panel panel1;
+            System.Windows.Forms.Panel panel2;
+            System.Windows.Forms.Panel panel3;
+            System.Windows.Forms.Panel panel4;
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.clock = new System.Windows.Forms.Label();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            panel1 = new System.Windows.Forms.Panel();
+            panel2 = new System.Windows.Forms.Panel();
+            panel3 = new System.Windows.Forms.Panel();
+            panel4 = new System.Windows.Forms.Panel();
+            panel3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.AutoSize = true;
+            panel1.BackColor = System.Drawing.Color.WhiteSmoke;
+            panel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            panel1.Cursor = System.Windows.Forms.Cursors.Default;
+            panel1.Location = new System.Drawing.Point(526, 12);
+            panel1.Name = "panel1";
+            panel1.Size = new System.Drawing.Size(152, 398);
+            panel1.TabIndex = 0;
+            panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // panel2
+            // 
+            panel2.AutoSize = true;
+            panel2.BackColor = System.Drawing.Color.WhiteSmoke;
+            panel2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            panel2.Cursor = System.Windows.Forms.Cursors.Default;
+            panel2.Location = new System.Drawing.Point(170, 222);
+            panel2.Name = "panel2";
+            panel2.Size = new System.Drawing.Size(350, 188);
+            panel2.TabIndex = 1;
+            // 
+            // panel3
+            // 
+            panel3.AutoSize = true;
+            panel3.BackColor = System.Drawing.Color.WhiteSmoke;
+            panel3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            panel3.Controls.Add(this.pictureBox2);
+            panel3.Controls.Add(this.pictureBox1);
+            panel3.Controls.Add(this.button2);
+            panel3.Controls.Add(this.button1);
+            panel3.Controls.Add(this.comboBox2);
+            panel3.Controls.Add(this.comboBox1);
+            panel3.Controls.Add(this.label3);
+            panel3.Controls.Add(this.label2);
+            panel3.Controls.Add(this.clock);
+            panel3.Cursor = System.Windows.Forms.Cursors.Default;
+            panel3.Location = new System.Drawing.Point(12, 12);
+            panel3.Name = "panel3";
+            panel3.Size = new System.Drawing.Size(152, 398);
+            panel3.TabIndex = 1;
+            // 
+            // clock
+            // 
+            this.clock.AutoSize = true;
+            this.clock.Font = new System.Drawing.Font("alarm clock", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.clock.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(106)))), ((int)(((byte)(149)))));
+            this.clock.Location = new System.Drawing.Point(23, 31);
+            this.clock.Name = "clock";
+            this.clock.Size = new System.Drawing.Size(108, 24);
+            this.clock.TabIndex = 0;
+            this.clock.Text = "00:00:00";
+            this.clock.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.clock.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // panel4
+            // 
+            panel4.AutoSize = true;
+            panel4.BackColor = System.Drawing.Color.WhiteSmoke;
+            panel4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            panel4.Cursor = System.Windows.Forms.Cursors.Default;
+            panel4.Location = new System.Drawing.Point(170, 12);
+            panel4.Name = "panel4";
+            panel4.Size = new System.Drawing.Size(350, 204);
+            panel4.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(14, 74);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 15);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "PORT:";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(14, 118);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(81, 15);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "BAUD RATE:\r\n";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(17, 136);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 23);
+            this.comboBox1.TabIndex = 3;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(17, 92);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 23);
+            this.comboBox2.TabIndex = 4;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Times New Roman", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(17, 165);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(121, 24);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "REFRESH";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Times New Roman", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(17, 195);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(121, 24);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "CONNECT";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(38, 301);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(81, 94);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 7;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.InitialImage = ((System.Drawing.Image)(resources.GetObject("pictureBox2.InitialImage")));
+            this.pictureBox2.Location = new System.Drawing.Point(38, 225);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(77, 70);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 8;
+            this.pictureBox2.TabStop = false;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(214)))), ((int)(((byte)(214)))), ((int)(((byte)(214)))));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.CausesValidation = false;
+            this.ClientSize = new System.Drawing.Size(700, 422);
+            this.Controls.Add(panel4);
+            this.Controls.Add(panel3);
+            this.Controls.Add(panel2);
+            this.Controls.Add(panel1);
+            this.Font = new System.Drawing.Font("Times New Roman", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
-            this.Text = "Phoenix";
+            this.Text = " ";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            panel3.ResumeLayout(false);
+            panel3.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.Label clock;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
--- Cansat_Pheonix/Form1.cs
+++ Cansat_Pheonix/Form1.cs
@@ -16,5 +16,40 @@ namespace Cansat_Pheonix
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timer1.Start();
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            clock.Text = DateTime.Now.ToString("HH:mm:ss");
+        }
     }
 }
